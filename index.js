af70d1f2b262a53f036f2e342eea25113a831dcc
Afran1984 <afranrazzak1984@gmail.com>
2025-01-20 15:36:13 +0600

    server maker & Mongo db connect

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ const port = 3300; // You can change the port number
 app.use(cors());
 app.use(express.json());
 
+
+// Password: mjL538g4i9QlTH0U
+// Name: Product-Hunt-admin
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.bqiq2nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 // Serve product data
 app.get("/api/products", (req, res) => {
   // Read the product data from the JSON file
