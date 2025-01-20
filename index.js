801416cf5786c1ff113f5f21fa32804a6707fdce
Afran1984 <afranrazzak1984@gmail.com>
2025-01-20 15:44:09 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.bqiq2nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://producthunt:bqfUxChuyzS5TnAD@cluster0.bqiq2nz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
